a2f7451ab80cfb7ff86e9db8ff0ac7084d91908d
Shahariar Rijon <shahariar.rijon@gmail.com>
2024-09-10 01:17:08 +0600

    initialized dash

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
-import Nav from "../componants/Nav/index.js"
-import Hero from '../componants/Hero/index.js'
+import Nav from "../components/Nav/index.js"
+import Hero from '../components/Hero/index.js'
 const Home = () => {
   return (
     <div className="max-w-screen-xl	mx-auto font-inter">
